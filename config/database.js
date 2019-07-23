3dc4b04fb3721b9f7e7e486f4f6f8bfe8e81d3f4
Aden Trisna Daud Kurnia <adentrisnadaudkurnia@gmail.com>
2019-07-23 16:19:27 +0700

    setting up heroku

--- config/database.js
+++ config/database.js
@@ -2,7 +2,8 @@
 
 /** @type {import('@adonisjs/framework/src/Env')} */
 const Env = use('Env')
-
+const Url = require('url-parse')
+const CLEARDB_DATABASE_URL = new Url(Env.get('CLEARDB_DATABASE_URL'))
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
 
@@ -73,11 +74,11 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.hostname),
+      port: Env.get('DB_PORT', CLEARDB_DATABASE_URL.port),
+      user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
+      password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
+      database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
     },
     debug: Env.get('DB_DEBUG', false)
   }
